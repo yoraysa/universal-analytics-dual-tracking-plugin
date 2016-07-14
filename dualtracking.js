e46fcb3093b94ed1e3c83bf59f609dbfd61e72a1
Yoray Sanela <yoraysa@gmail.com>
2016-07-14 04:28:39 +0200

    Debug messages and some small code changes
    Hi,
    that's my first github file change Propose.
    didn't check it though - please do.

--- dualtracking.js
+++ dualtracking.js
@@ -1,29 +1,29 @@
-    /**
+	/**
 	 *  Universal Analytics Dual Tracking Plugin
 	 */
 (function(){
 	function providePlugin(pluginName, pluginConstructor) {
-	  var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
-	  if (typeof ga == 'function') {
+		var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
+		if (typeof ga == 'function') {
 		ga('provide', pluginName, pluginConstructor);
 	  }
 	}
 
 	var DualTracking = function(tracker, config) {
-	  this.tracker = tracker;
-	  this.property = config.property, 
-	  this.isDebug = config.debug;
-	  this.transport = config.transport || 'beacon';
+		this.tracker = tracker;
+		this.property = config.property, 
+		this.isDebug = config.debug;
+		this.transport = config.transport || 'beacon';
 	};
 	
-    /**
+	/**
 	 * 
 	 */
 	DualTracking.prototype.doDualTracking = function() {
-		this.debugMessage('Initializing the dualtracking plugin for GA');  
+		this.debug('DualTracking: Init');
 		if(!this.property || !this.property.match(/^UA-([0-9]*)-([0-9]{1,2}$)/)){
-			this.debugMessage('dualtracking plugin: property id, needs to be set and have the following format UA-XXXXXXXX-YY');  
-			return 0;			
+			this.debug('DualTracking: Error. Bad Property ID Format (UA-XXXXXX-YY)');
+			return 0;		
 		}else{
 			window.__gaDualTracking = {};
 			window.__gaDualTracking.property = this.property;
@@ -32,15 +32,20 @@
 		
 		var originalSendHitTask = this.tracker.get('sendHitTask');
 		this.tracker.set('sendHitTask', function(model) {
-			var payLoad = model.get('hitPayload');	
+			this.debug('DualTracking: Init');
+			var payLoad = model.get('hitPayload');
 			var data = (payLoad).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
 			data.tid = window.__gaDualTracking.property;
+			
 			originalSendHitTask(model);
+			this.debug('DualTracking: Prepare Hit');
 			var newPayload = Object.keys(data).map(function(key) { return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]); }).join('&');
 			if(__gaDualTracking.transport=="image"){
-				var i=new Image(1,1);	
+				this.debug('DualTracking: Send Image Hit');
+				var i=new Image(1,1);
 				i.src="https://www.google-analytics.com/collect"+"?"+newPayload;i.onload=function(){return;}				
 			}else if(__gaDualTracking.transport=="beacon"){
+				this.debug('DualTracking: Send Beacon Hit');
 				navigator.sendBeacon("https://www.google-analytics.com/collect", newPayload);
 			}			
 		});
@@ -49,9 +54,9 @@
 	/**
 	 * Displays a debug message in the console, if debugging is enabled.
 	 */
-	DualTracking.prototype.debugMessage = function(message) {
-	  if (!this.isDebug) return;
-	  if (console) console.debug(message);
+	DualTracking.prototype.debug = function(message) {
+		if (this.isDebug && typeof console != 'undefined')
+			console.debug(message);
 	};
 
 	providePlugin('dualtracking', DualTracking);  	
